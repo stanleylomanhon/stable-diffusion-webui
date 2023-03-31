9724b79352b787881f236b74187df91ec441d028
Stanley Lo <85002118+stanleylomanhon@users.noreply.github.com>
2023-03-31 22:14:57 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON="C:\Users\stand\AppData\Local\Programs\Python\Python311\python.exe"
+set PYTHON="C:\Users\stand\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
